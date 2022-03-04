6f797f0092af9631e1269e05e5df8ccf390c3dc8
Ivy Swan <ivyswan@Ivys-MacBook-Pro.local>
2022-03-04 10:38:02 +0000

    adds countdown exercise using recursion

--- recursion.js
+++ recursion.js
@@ -31,3 +31,29 @@ function sum(arr, n) {
 sum([1], 0); // 0
 sum([2, 3, 4], 1); // 2
 sum([2, 3, 4], 1); // 9
+
+
+// **** Use Recursion to Create a Countup and Countdown ****
+// Countup
+function countup(n) {
+    if (n < 1) {
+        return [];
+    } else {
+        const countArray = countup(n - 1);
+        countArray.push(n);
+        return countArray;
+    }
+}
+// The value [1, 2, 3, 4, 5] will be displayed in the console
+console.log(countup(5));
+
+// Countdown
+function countdown(n) {
+    if (n < 1) {
+        return [];
+    } else {
+        const countArray = countdown(n - 1);
+        countArray.unshift(n);
+        return countArray;
+    }
+}

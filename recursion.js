6800f74e5dd86525c9445719db506d6be8ccce5d
Ivy Swan <ivyswan@Ivys-MacBook-Pro.local>
2022-03-07 11:50:59 +0000

    updates recursion exercise

--- recursion.js
+++ recursion.js
@@ -1,5 +1,4 @@
-// Replace Loops using Recursion
-
+// **** Replace Loops using Recursion ****
 function multiply(arr, n) {
     let product = 1;
     for (let i = 0; i < n; i++) {
@@ -17,15 +16,13 @@ function multiply(arr, n) {
     }
 }
 
-// exercise
+// Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr
 function sum(arr, n) {
-    // Only change code below this line
     if (n <= 0) {
         return 0;
     } else {
         return sum(arr, n - 1) + arr[n - 1];
     }
-    // Only change code above this line
 }
 
 sum([1], 0); // 0

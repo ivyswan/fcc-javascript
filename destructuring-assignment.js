9f6e5583ecd7ca9fc3c6f15180a12444e8bd4805
Ivy Swan <ivyswan@Ivys-MacBook-Pro.local>
2022-03-08 22:15:57 +0000

    adds Destructuring Assignment to Assign Variables from Nested Objects

--- destructuring-assignment.js
+++ destructuring-assignment.js
@@ -11,8 +11,29 @@ const HIGH_TEMPERATURES = {
     today: 77,
     tomorrow: 80
 };
-
 // const today = HIGH_TEMPERATURES.today;
 // const tomorrow = HIGH_TEMPERATURES.tomorrow;
-
 const { today, tomorrow } = HIGH_TEMPERATURES;
+
+// Destructuring Assignment to Assign Variables from Nested Objects
+const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+};
+
+// const lowToday = LOCAL_FORECAST.today.low;
+// const highToday = LOCAL_FORECAST.today.high;
+const { today: { low: lowToday, high: highToday } } = LOCAL_FORECAST;
+
+// Destructure values from nested objects
+const user = {
+    johnDoe: {
+        age: 34,
+        email: 'johnDoe@freeCodeCamp.com'
+    }
+};
+// extract the values of object properties and assign them to variables with the same name
+const { johnDoe: { age, email } } = user;
+// assign an object properties' values to variables with different names
+const { johnDoe: { age: userAge, email: userEmail } } = user;
